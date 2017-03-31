6c480eb057ff8f2eb988fcf5cc22a2f70c6f91d4
Rishabh <rishabhagarwal467@gmail.com>
2017-03-31 13:24:57 +0530

    Added some basic code in program.cc and procedure.cc

--- cs316-asgn6/procedure.cc
+++ cs316-asgn6/procedure.cc
@@ -8,20 +8,47 @@
 #include "procedure.hh"
 #include "program.hh"
 
+Procedure::Procedure(Data_Type proc_return_type, string proc_name, int line)
+{
+	return_type = proc_return_type;
+	name = proc_name;
+	lineno = line;
+	// sequence_ast = new Sequence_Ast(line);
 
-Procedure::Procedure(Data_Type proc_return_type, string proc_name, int line){};
-Procedure::~Procedure(){};
+};
+Procedure::~Procedure()
+{
+	delete sequence_ast;
+};
 
-string Procedure:: get_proc_name(){};
-void Procedure:: set_sequence_ast(Sequence_Ast & sa){};
-void Procedure:: set_local_list(Symbol_Table & new_list){};
-Data_Type Procedure::get_return_type(){};
-Symbol_Table_Entry & Procedure:: get_symbol_table_entry(string variable_name){};
+string Procedure::get_proc_name()
+{
+	return name;
+};
+void Procedure::set_sequence_ast(Sequence_Ast & sa)
+{
+	sequence_ast = &sa;
+};
+void Procedure::set_local_list(Symbol_Table & new_list)
+{
+	local_symbol_table = new_list;
+};
+Data_Type Procedure::get_return_type()
+{
+	return return_type;
+};
+Symbol_Table_Entry & Procedure::get_symbol_table_entry(string variable_name)
+{
+	return local_symbol_table.get_symbol_table_entry(variable_name);
+};
 
-void Procedure:: print(ostream & file_buffer){};
-void Procedure:: print_sym(ostream & file_buffer){};
+// void Procedure:: print(ostream & file_buffer){};
+// void Procedure:: print_sym(ostream & file_buffer){};
 
-bool Procedure:: variable_in_symbol_list_check(string variable){};
+bool Procedure::variable_in_symbol_list_check(string variable)
+{
+	return local_symbol_table.variable_in_symbol_list_check(variable);
+};
 
 // compile
 // void Procedure:: compile(){};
--- cs316-asgn6/program.cc
+++ cs316-asgn6/program.cc
@@ -9,23 +9,45 @@
 #include "program.hh"
 
 
+	// Symbol_Table global_symbol_table;
+	// Procedure * procedure;
+
+
 Program program_object;
 
 Program::Program(){};
 Program::~Program(){};
 void Program::delete_all(){};
 
-void Program::set_procedure(Procedure * proc, int line){};
-void Program::set_global_table(Symbol_Table & new_global_table){};
+void Program::set_procedure(Procedure * proc, int line)
+{
+	// procedure = 
+};
+void Program::set_global_table(Symbol_Table & new_global_table)
+{
+	global_symbol_table = new_global_table;
+};
 
-Symbol_Table_Entry & Program::get_symbol_table_entry(string variable){};
+Symbol_Table_Entry & Program::get_symbol_table_entry(string variable)
+{
+	return procedure->get_symbol_table_entry(variable);
+};
 
 void Program::print_sym(){};
 void Program::print(){};
 
-bool Program::variable_proc_name_check(string symbol){};
-bool Program::variable_in_symbol_list_check(string variable){};
-void Program::global_list_in_proc_check(){};
+bool Program::variable_proc_name_check(string symbol)
+{
+
+};
+bool Program::variable_in_symbol_list_check(string variable)
+{
+	// return  
+};
+void Program::global_list_in_proc_check()
+{
+	global_symbol_table.global_list_in_proc_map_check();
+};
 bool Program::variable_in_proc_map_check(string symbol){};
 
 // compile
